fa3bd632009b946a728c29c33e38d9cd967ea630
diskahaha <109210020+diskahaha@users.noreply.github.com>
2022-09-18 21:33:24 +0700

    Update main-menu1.js

--- plugins/main-menu1.js
+++ plugins/main-menu1.js
@@ -5,13 +5,12 @@ let date = moment.tz('Asia/Jakarta').format("dddd, Do MMMM, YYYY")
 
 conn.sendButtonDoc(m.chat, 'ʜᴀɪ,ᴀᴋᴜ ᴀᴅᴀʟᴀʜ ʙᴏᴛ ʏᴀɴɢ ᴅɪʙᴇʀɪ ɴᴀᴍᴀ 𝕭𝖔𝖙 • Andii ༢࿔ྀ,ᴀᴋᴜ ᴅɪ ᴋᴇᴍʙᴀɴɢᴋᴀɴ ᴏʟᴇʜ ꜱᴇꜱᴇᴏʀᴀɴɢ ʏᴀɴɢ ʙᴇʀɴᴀᴍᴀ 𝕭𝖔𝖙 • Andii ༢࿔ྀ,ʙᴇʟɪᴀᴜ ᴍᴀꜱɪʜ ᴘᴇᴍᴜʟᴀ ᴅᴀɴ ᴍᴀꜱɪʜ ʙᴇʟᴀᴊᴀʀ ʙᴀɴʏᴀᴋ ʜᴀʟ,ᴅᴀɴ ᴛᴜᴊᴜᴀɴɴʏᴀ ʜᴀɴʏᴀ ɪɴɢɪɴ ᴛᴀᴜ ʙᴇʀʙᴀɢᴀɪ ʜᴀʟ ʏᴀɴɢ ᴀᴅᴀ ᴅɪᴅᴜɴɪᴀ ʙᴏᴛ,ᴜɴᴛᴜᴋ ᴍᴇʟɪʜᴀᴛ ꜰɪᴛᴜʀ ʙᴏᴛ,ꜱɪʟᴀʜᴋᴀɴ ᴋʟɪᴋ ʙᴜᴛᴛᴏɴ ᴅɪʙᴀᴡᴀʜ\n\nɴᴏᴛᴇ: ʜᴀʀᴀᴘ ɢᴜɴᴀᴋᴀɴ ʙᴏᴛ ꜱᴇᴄᴀʀᴀ ʙɪᴊᴀᴋ ᴅᴀɴ ᴊᴀɴɢᴀɴ ᴅɪꜱᴘᴀᴍ', data.namabot, 'ʟɪꜱᴛᴍᴇɴᴜ', '.m', fake, { contextInfo: {
 externalAdReply :{
-    showAdAttribution: true,
+    showAdAttribution: false,
     mediaUrl: data.sc,
     mediaType: 2,
     sourceUrl: data.sc,
-    title: 'Jangan Dispam Ya Kak',
+    title: 'Grup Bot',
     body: wm,
-    thumbnail: await(await fetch(img)).buffer(),
 }}})
 }
 
