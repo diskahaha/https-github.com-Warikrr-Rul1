854f4dcf1895db692f0633ef9c7be715226466ca
diskahaha <109210020+diskahaha@users.noreply.github.com>
2022-09-18 20:23:24 +0700

    Update _allfake.js

--- plugins/_allfake.js
+++ plugins/_allfake.js
@@ -68,7 +68,7 @@ handler.all = async function (m) {
 	 { fromMe: false,
 	 participant: `0@s.whatsapp.net`, ...(m.chat  ? 
 	 { remoteJid: "status@broadcast" } : {}) },
-	 message: { "liveLocationMessage": { "caption":"by : Arull","h": `${wm}`, 'jpegThumbnail': fs.readFileSync('./src/logo.jpg')}}
+	 message: { "liveLocationMessage": { "caption":"by : 𝕭𝖔𝖙 ༢࿔ྀ","h": `${wm}`, 'jpegThumbnail': fs.readFileSync('./src/logo.jpg')}}
 	}
         
             const ftoko = {
